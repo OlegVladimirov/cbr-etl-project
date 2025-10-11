c4ee597ff6bb0c64b36d07d9f8eb589162493463
0 <oleg.txt@gmail.com>
2025-10-11 11:36:09 +0300

    encode text in utf-8

--- db/ddl.sql
+++ db/ddl.sql
@@ -1,15 +1,15 @@
-drop table if exists exchange_rates;
+DROP TABLE IF EXISTS exchange_rates;
 CREATE TABLE exchange_rates (
     date DATE,
     currency_code VARCHAR(10),
     rate DECIMAL(10, 4)
 );
 
-drop table if exists currency_reference;
+DROP TABLE IF EXISTS currency_reference;
 CREATE TABLE currency_reference (
     id VARCHAR(10),
-    name TEXT,
-    eng_name TEXT,
-    nominal INTEGER,
+    name TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+    eng_name TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+    nominal INT,
     iso_char_code CHAR(3)
-);
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
